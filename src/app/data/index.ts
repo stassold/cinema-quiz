0cfd21fb743acf9ea065f762b6066e994ce87fb1
stassold <Stason-Kobzon@mail.ru>
2023-07-05 16:36:26 +0700

    Move questions to a separate variable

---
+++ src/app/data/index.ts
@@ -0,0 +1,17 @@
+import {Question} from "app/types";
+
+export const questions: Question[] = [
+    {
+        text: "Какой язык программирования используется для разработки мобильных приложений под Android?",
+        answers: ["Java", "Swift", "C#", "JavaScript"],
+        correctAnswerIndex: 0,
+        correctUserResponse: false
+    },
+    {
+        text: "Какой язык программирования используется для создания Web-страниц?",
+        answers: ["Java", "Swift", "C#", "JavaScript"],
+        correctAnswerIndex: 3,
+        correctUserResponse: false
+    },
+    // Добавьте другие вопросы
+];
